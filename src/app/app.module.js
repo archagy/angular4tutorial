4cfca6947359a3659280c4e4d69a3eefcfb14c83
Avillegas <alexiseliudrdz@gmail.com>
2017-05-29 23:42:14 -0500

    Star HTTP - https://angular.io/docs/ts/latest/tutorial/toh-pt6.html

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -8,7 +8,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
+var router_1 = require("@angular/router");
 var app_component_1 = require("./app.component");
+var dashboard_component_1 = require("./dashboard.component");
+var hero_detail_component_1 = require("./hero-detail.component");
+var heroes_component_1 = require("./heroes.component");
+var hero_service_1 = require("./hero.service");
+var routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: dashboard_component_1.DashboardComponent },
+    { path: 'detail/:id', component: hero_detail_component_1.HeroDetailComponent },
+    { path: 'heroes', component: heroes_component_1.HeroesComponent }
+];
 var AppModule = (function () {
     function AppModule() {
     }
@@ -18,10 +29,22 @@ AppModule = __decorate([
     core_1.NgModule({
         imports: [
             platform_browser_1.BrowserModule,
-            forms_1.FormsModule
+            forms_1.FormsModule,
+            router_1.RouterModule.forRoot(routes)
         ],
-        declarations: [app_component_1.AppComponent],
-        bootstrap: [app_component_1.AppComponent],
+        exports: [router_1.RouterModule],
+        declarations: [
+            app_component_1.AppComponent,
+            dashboard_component_1.DashboardComponent,
+            hero_detail_component_1.HeroDetailComponent,
+            heroes_component_1.HeroesComponent
+        ],
+        providers: [
+            hero_service_1.HeroService
+        ],
+        bootstrap: [
+            app_component_1.AppComponent
+        ],
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import {DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes',     component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [ 
-  			BrowserModule,
-  			FormsModule
-   ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
+	imports: [ 
+		BrowserModule,
+		FormsModule,
+		RouterModule.forRoot(routes)
+	],
+	exports: [ RouterModule ],
+	declarations: [ 
+  		AppComponent,
+  		DashboardComponent,
+  		HeroDetailComponent,
+  		HeroesComponent
+  		
+  	],
+  	providers: [
+    	HeroService
+  	],
+	bootstrap:    [ 
+  		AppComponent 
+  	],
 })
+
 export class AppModule { }
